676b1561556f9750ef3579ca3166a1f15d4d146c
Follow the Four Leafed Clover 🍀 <55417207+followthefourleafedclover@users.noreply.github.com>
2024-05-07 20:29:29 -0400

    updates: hub made, server exports in folders

---
+++ Server.py
@@ -0,0 +1,165 @@
+import discord 
+import pandas as pd 
+import os 
+
+class Server:
+    def __init__(self, client: discord.Client, guild: discord.Guild, *args):
+        self.__args = [x for x in args]
+
+        if not any([isinstance(x, str) for x in self.__args]):
+            raise TypeError
+
+        self.__args_values = [[] for x in self.__args] 
+        self.__client = client 
+        self.guild = guild
+
+        self.options = {'members': False, 'messages': False}
+
+        if 'members' in self.__args:
+            self.options['members'] = True
+        if 'messages' in self.__args:
+            self.options['messages'] = True
+
+        for index, arg in enumerate(self.__args):
+
+            gen_function_code_getter = f'''async def {self.guild.capitalize()}Get{arg.capitalize()}():
+            return self._Server__args_values[{index}]'''
+
+            gen_function_code_setter = f'''async def {self.guild.capitalize()}Set{arg.capitalize()}(value):
+            if not isinstance(value, list):
+                raise TypeError("value is supposed to be type list not " + str(type(value)))
+            self._Server__args_values[{index}] = value 
+            '''
+
+            gen_function_code_add = f'''async def {self.guild.capitalize()}Add{arg.capitalize()}(value):
+            self._Server__args_values[{index}].append(value)'''
+
+            gen_function_code_remove = f'''async def {self.guild.capitalize()}Remove{arg.capitalize()}(*args):
+            if len(args) > 1:
+                raise TypeError("remove{arg.capitalize()} only takes one positional agrument but " + str(len(args)) + " were given")
+            if args:
+                del self._Server__args_values[{index}][args[0]]
+            else:
+                del self._Server__args_values[{index}][-1]'''
+
+            
+            exec(gen_function_code_getter, {'self':self}, globals())
+            exec(gen_function_code_setter, {'self':self}, globals())
+            exec(gen_function_code_add, {'self':self}, globals())
+            exec(gen_function_code_remove, {'self':self}, globals())
+
+        self.server_dir = f"{os.getcwd()}/{self.guild.capitalize()}"
+
+
+    async def __generic_function(self):
+        print(f"Taking Snapshot of {self.guild.capitalize()} . . .")
+
+    def __get_guild_attrs(self):
+        guild_attrs = dir(discord.Guild)
+        start_index = 0
+        for index, attrs in enumerate(guild_attrs):
+            if not '_' in attrs:
+                start_index = index 
+                break 
+        return guild_attrs[start_index:]
+    
+    def take_snapshot(self, func=None):
+        async def take_snapshot_wrapper():
+
+            self.guild_names = [x.name for x in self.__client.guilds]
+
+            self.guild_obj = self.__client.guilds[self.guild_names.index(self.guild)] 
+
+            global_guild_exec_code1 = f"global {self.guild.capitalize()}"
+            global_guild_exec_code2  = f"{self.guild.capitalize()} = self.guild_obj"
+            
+            exec(global_guild_exec_code1, {'self':self}, globals())
+            exec(global_guild_exec_code2, {'self':self}, globals())
+
+            self.guild_attrs = self.__get_guild_attrs()
+
+            for attr in self.guild_attrs:
+                code = f'{self.guild.capitalize()}{attr.capitalize()} = self.guild_obj.{attr}'
+                exec(code, {'self':self}, globals())
+        
+            if self.options['members']:
+                for member in self.guild_obj.members:
+                    await eval(f"{self.guild.capitalize()}AddMembers('{str(member)}')", globals())
+
+            if self.options['messages']:
+                for channel in self.guild_obj.channels:
+                    try:
+                        async for message in channel.history(limit=None):
+                            await eval(f"{self.guild.capitalize()}AddMessages('{message.content}')", globals())
+                    except AttributeError:
+                        pass
+
+            if func is None:
+                await self.__generic_function() 
+            else:    
+                await func()
+
+        if func is None:
+            return take_snapshot_wrapper()
+        return take_snapshot_wrapper
+
+    def file_type_manager(self, file_type, frame, text):
+        if file_type ==  'csv':
+            return frame.to_csv(os.path.join(self.server_dir, f'{text}.csv'), index=False)
+
+        if file_type == 'excel':
+            return frame.to_excel(os.path.join(self.server_dir, f'{text}.xlsx'), sheet_name=f"{text}", index=False)
+        raise NotImplementedError
+
+
+    async def export(self, file_type='csv'):
+        print(self.server_dir)
+        if not os.path.isdir(self.server_dir):
+            os.mkdir(self.server_dir)
+        self.__args_zipped = dict(zip(self.__args, self.__args_values))
+
+        async def merge():
+            lengths = [len(x) for x in self.__args_zipped.values()] 
+            lengths_zipped = list(zip(self.__args, lengths))
+            sorted(lengths_zipped, key=lambda x: x[1])
+            to_merge = [] 
+            temp = []
+            for i in range(len(lengths_zipped) - 1):
+                if lengths_zipped[i][1] == lengths_zipped[i+1][1]:
+                    if not lengths_zipped[i][0] in temp:
+                        temp.append(lengths_zipped[i][0])
+                    if not lengths_zipped[i+1][0] in temp:
+                        temp.append(lengths_zipped[i+1][0])         
+                else:
+                    if temp:
+                        to_merge.append(temp) 
+                        temp = [] 
+            if temp:
+                to_merge.append(temp)    
+            return to_merge
+
+        to_merge = await merge() 
+    
+        if to_merge:
+            df_dict = {} 
+            for index, group in enumerate(to_merge): 
+                for item in group: 
+                    df_dict.update({item: self.__args_zipped[item]})
+
+                print(df_dict)
+                df = pd.DataFrame(df_dict)
+                self.file_type_manager(file_type, df, f"{'&'.join(group)}") 
+                #df.to_csv(f"{'&'.join(group)}.csv")
+
+            flatten = [y for y in [x for x in to_merge]][0]
+            rest = [x for x in self.__args if not x in flatten]
+            
+            for item in rest:
+                df = pd.DataFrame({item: self.__args_zipped[item]})
+                self.file_type_manager(file_type, df, f"{item}")
+
+        else:
+            for zipped in self.__args_zipped:
+                df = pd.DataFrame({zipped: self.__args_zipped[zipped]})
+                self.file_type_manager(file_type, df, f"{zipped}") 
+                #df.to_csv(f"{zipped}.csv")
--- main.py
+++ main.py
@@ -4,162 +4,25 @@ import dotenv
 import nest_asyncio
 import os
 import pandas as pd
-import warnings
 from bot import Bot
+import sys
+from Hub import Hub
 
 nest_asyncio.apply()
 dotenv.load_dotenv()
 
 
-class Server:
-    def __init__(self, client: discord.Client, guild: discord.Guild, *args):
-        self.__args = [x for x in args]
+intents=discord.Intents.all()
+intents.typing = True
+intents.messages = True
+intents.message_content = True
+bot = Bot(command_prefix='/', intents=intents)
+base = Hub(bot, "messages", 'members')
 
-        if not any([isinstance(x, str) for x in self.__args]):
-            raise TypeError
-
-        self.__args_values = [[] for x in self.__args] 
-        self.__client = client 
-        self.guild = guild
-
-        self.options = {'members': False, 'messages': False}
-
-        if 'members' in self.__args:
-            self.options['members'] = True
-        if 'messages' in self.__args:
-            self.options['messages'] = True
-
-        for index, arg in enumerate(self.__args):
-
-            gen_function_code_getter = f'''async def get{arg.capitalize()}():
-            return self._Server__args_values[{index}]'''
-
-            gen_function_code_setter = f'''async def set{arg.capitalize()}(value):
-            if not isinstance(value, list):
-                raise TypeError("value is supposed to be type list not " + str(type(value)))
-            self._Server__args_values[{index}] = value 
-            '''
-
-            gen_function_code_add = f'''async def add{arg.capitalize()}(value):
-            self._Server__args_values[{index}].append(value)'''
-
-            gen_function_code_remove = f'''async def remove{arg.capitalize()}(*args):
-            if len(args) > 1:
-                raise TypeError("remove{arg.capitalize()} only takes one positional agrument but " + str(len(args)) + " were given")
-            if args:
-                del self._Server__args_values[{index}][args[0]]
-            else:
-                del self._Server__args_values[{index}][-1]'''
-
-            
-            exec(gen_function_code_getter, {'self':self}, globals())
-            exec(gen_function_code_setter, {'self':self}, globals())
-            exec(gen_function_code_add, {'self':self}, globals())
-            exec(gen_function_code_remove, {'self':self}, globals())
-
-    async def __generic_function(self):
-        print("Taking Snapshot of Server . . .")
-
-    def __get_guild_attrs(self):
-        guild_attrs = dir(discord.Guild)
-        start_index = 0
-        for index, attrs in enumerate(guild_attrs):
-            if not '_' in attrs:
-                start_index = index 
-                break 
-        return guild_attrs[start_index:]
-    
-    def take_snapshot(self, func=None):
-        async def take_snapshot_wrapper():
-
-            self.guild_names = [x.name for x in self.__client.guilds]
-
-            self.guild_obj = self.__client.guilds[self.guild_names.index(self.guild)]
-
-            global Guild 
-            Guild  = self.guild_obj  
-
-            self.guild_attrs = self.__get_guild_attrs()
-
-            for attr in self.guild_attrs:
-                code = f'{attr} = self.guild_obj.{attr}'
-                exec(code, {'self':self}, globals())
-        
-            if self.options['members']:
-                for member in self.guild_obj.members:
-                    await addMembers(member)
-
-            if self.options['messages']:
-                for channel in self.guild_obj.channels:
-                    try:
-                        async for message in channel.history(limit=None):
-                            await addMessages(message.content)
-                    except:
-                        pass
-
-            if func is None:
-                await self.__generic_function() 
-            else:    
-                await func()
-
-        if func is None:
-            return take_snapshot_wrapper()
-        return take_snapshot_wrapper
-
-
-    async def export(self, file_type='csv'):
-        self.__args_zipped = dict(zip(self.__args, self.__args_values))
-
-        def merge():
-            lengths = [len(x) for x in self.__args_zipped.values()] 
-            print(lengths)
-            lengths_zipped = list(zip(self.__args, lengths))
-            sorted(lengths_zipped, key=lambda x: x[1])
-            to_merge = [] 
-            temp = []
-            for i in range(len(lengths_zipped) - 1):
-                if lengths_zipped[i][1] == lengths_zipped[i+1][1]:
-                    if not lengths_zipped[i][0] in temp:
-                        temp.append(lengths_zipped[i][0])
-                    if not lengths_zipped[i+1][0] in temp:
-                        temp.append(lengths_zipped[i+1][0])         
-                else:
-                    if temp:
-                        to_merge.append(temp) 
-                        temp = [] 
-            if temp:
-                to_merge.append(temp)    
-            return to_merge
-
-        to_merge = merge() 
-        print(to_merge)
-    
-
-        if to_merge:
-            df_dict = {} 
-            for index, group in enumerate(to_merge): 
-                for item in group: 
-                    df_dict.update({item: self.__args_zipped[item]})
-
-                print(df_dict)
-                df = pd.DataFrame(df_dict)
-                df.to_csv(f"{'&'.join(group)}.csv")
-
-            flatten = [y for y in [x for x in to_merge]][0]
-            rest = [x for x in self.__args if not x in flatten]
-            
-            for item in rest:
-                df = pd.DataFrame({item: self.__args_zipped[item]})
-                df.to_csv(f"{item}.csv")
-
-        else:
-            for zipped in self.__args_zipped:
-                df = pd.DataFrame({zipped: self.__args_zipped[zipped]})
-                df.to_csv(f"{zipped}.csv")
-
-bot = Bot(command_prefix='/', intents=discord.Intents.all())
+'''
 server = Server(bot, "monolith", "messages", "members", "test")
-
+server2 = Server(bot, "monolith2", "messages", "members", "test")
+'''
 
 @bot.event
 async def on_ready():
@@ -168,27 +31,39 @@ async def on_ready():
     print(bot.user.id)
     #await server.add('hel')
     #await server.take_snapshot()
+    @base.intialize
+    async def new():
+        print("no snaps")
+    
+    await new()
+    #await base.intialize()
+
+    #print(globals())
+    '''
     @server.take_snapshot
     async def w():
-        await setTest([1, 2])
+        await MonolithSetTest([1, 2])
         
     await w()
+    '''
+    #await server2.take_snapshot()
+    '''
     print(id)
-    print(Guild)
+    print(Monolith)
 
-    await server.export()
+    #await server.export()
+           
 
-    '''
-    messages = await getMessages()
-    print(messages)z
-    '''
+    messages = await MonolithGetMessages()
+    print(messages)    
 
+    messages = await Monolith2GetMessages()
+    print(messages)     
     
     
-    
-    members = await getMembers() 
+    members = await MonolithGetMembers() 
     print(members)
-
+    '''
 @bot.command()
 async def answer(ctx):
     await ctx.send("hello")
